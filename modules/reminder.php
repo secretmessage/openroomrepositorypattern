c6876c663561c54a7562c09d11646a1bdb3084e1
Kushal Hada <kushaldeveloper@gmail.com>
2017-07-31 10:54:09 -0400

    delete everything

--- modules/reminder.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (trim(model\Setting::fetchValue(\model\Db::getInstance(), 'remindermessage')) != "") {
-    $reminder_string = htmlentities(model\Setting::fetchValue(\model\Db::getInstance(), 'remindermessage'), ENT_COMPAT, 'UTF-8');
-    echo "<div id=\"remindermessage\"><span class=\"remindermessage\">" . $reminder_string . "</span></div>";
-}
